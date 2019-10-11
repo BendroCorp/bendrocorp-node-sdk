42675c5d7a6682122e6e2a748855dec3d306672b
Dale Myszewski II <dale.myszewski@gmail.com>
2019-10-11 16:09:13 -0500

    SC

--- lib/base.resource.ts
+++ lib/base.resource.ts
@@ -4,11 +4,10 @@ import { ApiClient } from './api-client';
 export class BaseResource {
   authClient: AuthClient;
   apiClient: ApiClient;
-  useProduction: boolean;
+  readonly useProduction: boolean;
   constructor(public params: { auth?: AuthClient, useProduction?: boolean }) {
     this.authClient = params.auth;
-    // this.useProduction = (this.useProduction) ? true : false;
-    if (this.useProduction == null) {
+    if (params.useProduction == null) {
       this.useProduction = true;
     } else {
       this.useProduction = params.useProduction;

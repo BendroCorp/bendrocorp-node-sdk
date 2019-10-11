39f633b72e1ed8be8a42662b1102a2a597fcf060
Dale Myszewski II <dale.myszewski@gmail.com>
2019-10-11 16:24:35 -0500

    SC

--- lib/event-stream.resource.ts
+++ lib/event-stream.resource.ts
@@ -1,4 +1,4 @@
-import { Channel, ActionCableService }  from 'angular2-actioncable';
+import { Channel, ActionCableService } from 'angular2-actioncable';
 import { Observable } from 'rxjs';
 import { StreamEvent } from './models/stream-event.model';
 import { BaseResource } from './base.resource';
@@ -12,19 +12,22 @@ export class StreamResource extends BaseResource {
   private streamConfig: BendroConfiguration;
   channel: Channel;
 
-  constructor(public params: { auth: AuthClient, useProduction?: boolean  }, private cableService: ActionCableService)
+  constructor(public params: { auth: AuthClient, useProduction?: boolean }, private cableService: ActionCableService)
   {
     super(params);
 
-    this.streamConfig =  (this.useProduction) ? new BendroConfiguration({ service: 'main', forWs: true }) : new BendroConfiguration({ service: 'local', forWs: true })
+    this.streamConfig = (this.useProduction) ? new BendroConfiguration({ service: 'main', forWs: true }) : new BendroConfiguration({ service: 'local', forWs: true })
   }
   
   /**
-   * 
+   * Get the event stream
    */
   get(): Observable<StreamEvent> {
+    const fullCableUri = `${this.streamConfig.serviceUri}?token=${this.authClient.getCredentials().access_token}`;
+    console.log(`Cable service URI: ${fullCableUri}`);
+    
     this.channel = this.cableService
-          .cable(`${this.streamConfig.serviceUri}?token=${this.authClient.getCredentials().access_token}`)
+          .cable(fullCableUri)
           .channel('EventChannel');
     return this.channel.received();
   }

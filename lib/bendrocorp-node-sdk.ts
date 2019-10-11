b73930dbe1a8f9abbbdf6f4da0c513ee4fd08c9f
Dale Myszewski II <dale.myszewski@gmail.com>
2019-10-11 14:11:38 -0500

    :smile:

--- lib/bendrocorp-node-sdk.ts
+++ lib/bendrocorp-node-sdk.ts
@@ -1,3 +1,5 @@
-export class BaseSdk {
-  
-}
+export { AuthClient } from './auth-client';
+export { userResource } from './user.resource';
+export { streamResource } from './event-stream.resource';
+export { eventResource } from './event.resource';
+export { reportResource } from './report.resource'
